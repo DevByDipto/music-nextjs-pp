7509c62418ecc31a3a42ad7c2ab7e696d5f29cf1
DevByDipto <dipto.1900@gmail.com>
2025-07-31 13:44:12 +0600

    added course page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FaeturedCourses from "@/components/FaeturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
@@ -15,6 +16,7 @@ export default function Home() {
     <TestimonialCards/>
     <UpCommingWebinars/>
     <Instructors/>
+    <Footer/>
    </main>
   );
 }
